4c6df140ab51ed7554c0a4776909bfc738c3f41c
OksanaKogutych <oksanakogutych97@gmail.com>
2021-11-10 11:10:27 +0200

    perfect-slider

--- src/js/index.js
+++ src/js/index.js
@@ -7,11 +7,14 @@ import { tns } from "../../node_modules/tiny-slider/src/tiny-slider"
 
 var slider = tns({
     container: '.banner__slider',
-    items: 1,
+    //items: 1,
+    loop: false,
     controlsContainer: "#customize-controls",
-    slideBy: 1,
+    //slideBy: 1,
     nav: true,    
     navContainer: "#customize-paginations",
+    autoplay: true,
+    autoplayButtonOutput: false,
     
   });
 
